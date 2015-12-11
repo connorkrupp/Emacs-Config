3aa36f5ff91617a45e14896160a719598416ae0d
Connor Krupp <connorkrupp@icloud.com>
2015-12-11 13:15:58 -0500

    Added theme, moved auto-saves/backups, setup-irony

--- init.el
+++ init.el
@@ -4,12 +4,22 @@
 (package-initialize)
 
 (setq gc-cons-threshold 100000000)
+(setq inhibit-startup-message t)
+
+(load-theme 'atom-one-dark t)
 
 (global-set-key [(control -)] 'undo)
 (delete-selection-mode t)
 (defalias 'yes-or-no-p 'y-or-n-p)
 (custom-set-variables
   '(line-number-mode t))
+
+;; store all backup and autosave files in the tmp dir
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+
 
 (defconst package-list
   '(irony
@@ -26,9 +36,24 @@
 
 (install-packages)
 
-(add-to-list 'load-path "~/.emacs.d/custom")
 
-(require 'setup-irony)
+(add-to-list 'load-path "~/.emacs.d/custom/")
+
+;;(require 'setup-irony)
+
+(add-hook 'c++-mode-hook 'irony-mode)
+(add-hook 'c-mode-hook 'irony-mode)
+(add-hook 'objc-mode-hook 'irony-mode)
+
+;; replace the `completion-at-point' and `complete-symbol' bindings in
+;; irony-mode's buffers by irony-mode's function
+(defun my-irony-mode-hook ()
+  (define-key irony-mode-map [remap completion-at-point]
+    'irony-completion-at-point-async)
+  (define-key irony-mode-map [remap complete-symbol]
+    'irony-completion-at-point-async))
+(add-hook 'irony-mode-hook 'my-irony-mode-hook)
+(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
 
 ;; Package: yasnippet
 (require 'yasnippet)
